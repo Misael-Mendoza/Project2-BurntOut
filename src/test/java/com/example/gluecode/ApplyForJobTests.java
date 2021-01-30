b90e48193106a70bc3c06ce51fb22e8b4821ae0a
Olga Melnikova <olgamelnikoff@gmail.com>
2021-01-30 12:46:36 -0500

    Selenium Testing For Candidates Finished

--- src/test/java/com/example/gluecode/ApplyForJobTests.java
+++ src/test/java/com/example/gluecode/ApplyForJobTests.java
@@ -26,31 +26,31 @@ public class ApplyForJobTests {
 	public ApplicationPage ap;
 	public ViewApplications vap;
 
-		@Given("a user is at the welcome page of BurntOut")
-		public void a_user_is_at_the_welcome_page_of_burnt_out() throws InterruptedException {
-		    TimeUnit.SECONDS.sleep(1);
-		    this.wp = new WelcomePage(BurntOutDriverUtility.driver);
-		    assertEquals(this.wp.header.getText(), "Welcome to BurntOut!");
-		}
-		
-		@When("a user inputs clicks the Sign in button")
-		public void a_user_inputs_clicks_the_sign_in_button() {
-			this.wp.signInButton.click();
-		}
-		@Then("the user is redirected to the login page")
-		public void the_user_is_redirected_to_the_login_page() throws InterruptedException {
-			TimeUnit.SECONDS.sleep(1);
-			this.lp = new LogIn(BurntOutDriverUtility.driver);
-			assertEquals(this.lp.header.getText(), "Welcome to BurntOut!");
-		}
-		@When("the user is at the Log in page")
-		public void the_user_is_at_the_log_in_page() {
-			assertEquals(this.lp.header.getText(), "Welcome to BurntOut!");
-		}
-		@When("the user enters their {string} and {string}")
-		public void the_user_enters_their_and(String string, String string2) {
-		    this.lp.logIn(string, string2);
-		}
+	/*@Given("a user is at the welcome page of BurntOut")
+	public void a_user_is_at_the_welcome_page_of_burnt_out() throws InterruptedException {
+	    TimeUnit.SECONDS.sleep(1);
+	    this.wp = new WelcomePage(BurntOutDriverUtility.driver);
+	    assertEquals(this.wp.header.getText(), "Welcome to BurntOut!");
+	}
+	
+	@When("a user inputs clicks the Sign in button")
+	public void a_user_inputs_clicks_the_sign_in_button() {
+		this.wp.signInButton.click();
+	}
+	@Then("the user is redirected to the login page")
+	public void the_user_is_redirected_to_the_login_page() throws InterruptedException {
+		TimeUnit.SECONDS.sleep(1);
+		this.lp = new LogIn(BurntOutDriverUtility.driver);
+		assertEquals(this.lp.header.getText(), "Welcome to BurntOut!");
+	}
+	@When("the user is at the Log in page")
+	public void the_user_is_at_the_log_in_page() {
+		assertEquals(this.lp.header.getText(), "Welcome to BurntOut!");
+	}
+	@When("the user enters their {string} and {string}")
+	public void the_user_enters_their_and(String string, String string2) {
+	    this.lp.logIn(string, string2);
+	}*/
 	
 	@Then("the user is redirected to the candidate screen")
 	public void the_user_is_redirected_to_the_candidate_screen() throws InterruptedException {
